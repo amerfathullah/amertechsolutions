205272a8fe8b6abc34352aabc7c47dbb92112b66
amerfathullah <amer@tfx.global>
2024-08-21 12:38:13 +0800

    install vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
-   site: 'https://amerfathullah.com',
-  integrations: [tailwind(),  sitemap()]
+  site: 'https://amerfathullah.com',
+  integrations: [tailwind(),  sitemap()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
