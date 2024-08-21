b2fdcaf13f5940065f2dbd1837aee997aebd1669
amerfathullah <amer@tfx.global>
2024-08-21 13:05:16 +0800

    configure sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
-  site: 'https://amerfathullah.com',
+  site: 'https://amertechsolutions.amerfathullah.com/',
   integrations: [tailwind(),  sitemap()],
   output: 'server',
   adapter: vercel({
