d6869e604741fd65c5d831ef7618424a02e20a3e
amerfathullah <amer@tfx.global>
2024-08-21 14:49:08 +0800

    configure PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import vercel from '@astrojs/vercel/serverless';
+import AstroPWA from '@vite-pwa/astro';
 export default defineConfig({
   site: 'https://amertechsolutions.amerfathullah.com/',
-  integrations: [tailwind(),  sitemap()],
+  integrations: [tailwind(),  sitemap(), AstroPWA()],
   output: 'server',
   adapter: vercel({
     webAnalytics: { enabled: true }
